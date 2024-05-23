d94ecf9f7a62de16ab73a8b8bfa0c4789b9d5b28
alprnkvgc <alperen.kavgaci@hotmail.com>
2024-05-23 11:25:03 +0300

    migrations

---
+++ Migrations/20240522013047_UpdatedEmployeeTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NowadaysIssueTracking.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedEmployeeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Employees",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BirthYear",
+                table: "Employees",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthYear",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Employees",
+                newName: "Name");
+        }
+    }
+}
