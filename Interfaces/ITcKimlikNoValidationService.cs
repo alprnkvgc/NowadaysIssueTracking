b35d46370f0f5165d3948a932f086db2271cce38
alprnkvgc <alperen.kavgaci@hotmail.com>
2024-05-23 11:21:50 +0300

    added tckimlik validation soap entegration and created service

---
+++ Interfaces/ITcKimlikNoValidationService.cs
@@ -0,0 +1,8 @@
+﻿namespace NowadaysIssueTracking.Interfaces
+{
+    public interface ITcKimlikNoValidationService
+    {
+        Task<bool> ValidateAsync(string tcKimlikNo, string firstName, string lastName, int birthYear);
+    }
+
+}
---
+++ Services/TcKimlikNoValidationService.cs
@@ -0,0 +1,34 @@
+﻿using NowadaysIssueTracking.Interfaces;
+using TCSorgu;
+
+namespace NowadaysIssueTracking.Services;
+
+public class TcKimlikNoValidationService : ITcKimlikNoValidationService
+{
+    private readonly KPSPublicSoapClient _client;
+
+    public TcKimlikNoValidationService()
+    {
+        _client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+    }
+
+    public async Task<bool> ValidateAsync(string tcKimlikNo, string firstName, string lastName, int birthYear)
+    {
+        try
+        {
+            var tcNo = Convert.ToInt64(tcKimlikNo);
+            var ad = firstName.ToUpper();
+            var soyad = lastName.ToUpper();
+
+            TCKimlikNoDogrulaResponse response = await _client.TCKimlikNoDogrulaAsync(tcNo, ad, soyad, birthYear);
+
+            return response.Body.TCKimlikNoDogrulaResult;
+        }
+        catch (Exception ex)
+        {
+            // Hata işleme
+            Console.WriteLine($"Error validating TC Kimlik No: {ex.Message}");
+            return false;
+        }
+    }
+}
